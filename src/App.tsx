47325ec06766147a80231066c54997b1f6359626
Hesham AbdAllah <hesham.abdallah.ha@gmail.com>
2021-06-22 22:57:03 +0200

    add show random data button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 
+import { useState } from 'react'
 import { StackedAreaChart } from './components'
 import salesData from './sales.json'
 
 export const App = () => {
+  const [stackedAreaChartData, setStackedAreaChartData] = useState<Array<object>>(salesData)
+
+  const onClickShowRandomData = () => {
+    setStackedAreaChartData(salesData.map((item) => ({
+      ...item,
+      sales: item.sales * Math.random()
+    })))
+  }
+
   return (
     <div className="app-container">
       <div className="container">
+        <button
+          className="showRandomDataButton"
+          onClick={onClickShowRandomData}
+        >
+          Show random data
+        </button>
+
         <StackedAreaChart
-          data={salesData}
           yAxisKey="sales"
           xAxisKey="period"
           seriesNameKey="channel"
+          data={stackedAreaChartData}
           title="Sales Forecast in National Market"
         />
       </div>
