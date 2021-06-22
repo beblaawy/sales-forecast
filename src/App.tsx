bffa967cec28f683005dba0fc311918c9d244ec8
Hesham AbdAllah <hesham.abdallah.ha@gmail.com>
2021-06-22 20:17:04 +0200

    use the stacked area chart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 
+import { StackedAreaChart } from './components'
+import salesData from './sales.json'
+
 export const App = () => {
   return (
-    <div className="App">
-      Hello
+    <div className="app-container">
+      <div className="container">
+        <StackedAreaChart
+          data={salesData}
+          yAxisKey="sales"
+          xAxisKey="period"
+          seriesNameKey="channel"
+          title="Sales Forecast in National Market"
+        />
+      </div>
     </div>
   )
 }
